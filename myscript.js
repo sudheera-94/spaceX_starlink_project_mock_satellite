c20db75ebe9d88221ffbabbd11801eefc643b452
sudheera-94 <sudheerawithanage@gmail.com>
2020-12-15 21:52:17 +0530

    Health and position update complete

--- myscript.js
+++ myscript.js
@@ -2,8 +2,25 @@ $(document).ready(function () {
 
     function sendReducedHealth(data) {
         $.each(data, function (index, item) {
-            if (item.healthPercentage > 10) {
-                item.healthPercentage = item.healthPercentage - 10
+            if (item.healthPercentage >= 10) {
+                item.healthPercentage = item.healthPercentage - 10;
+
+                item.xCoordinate = item.xCoordinate + item.xVelocity;
+                if (item.xCoordinate > 11) {
+                    item.xCoordinate = (item.xCoordinate % 12);
+                } else if (item.xCoordinate < 0) {
+                    item.xCoordinate = item.xCoordinate % 12;
+                    item.xCoordinate = 12 + item.xCoordinate;
+                }
+
+                item.yCoordinate = item.yCoordinate + item.yVelocity;
+                if (item.yCoordinate > 11) {
+                    item.yCoordinate = item.yCoordinate % 12;
+                } else if (item.yCoordinate < 0) {
+                    item.yCoordinate = item.yCoordinate % 12;
+                    item.yCoordinate = 12 + item.yCoordinate;
+                }
+
                 $.ajax({
                     type: 'POST',
                     url: 'http://127.0.0.1:8002/healthCheck/',
@@ -38,7 +55,6 @@ $(document).ready(function () {
             poll();
         }, 20000);
 
-    // poll(); // Runs immediately without delays
 });
 
 
